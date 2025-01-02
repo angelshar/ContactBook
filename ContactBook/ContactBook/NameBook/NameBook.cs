cd0c167519d7934e9c5bb149c173c2b078beb0a8
Angelina Harvey-Patrick <angelshpat@gmail.com>
2025-01-01 21:01:22 -0800

    Added Blank Solution, Added NameBook Class Library, Added NameBook Class Tests, Added Pseduocode, Added First Test Cases

---
+++ ContactBook/ContactBook/NameBook/NameBook.cs
@@ -0,0 +1,14 @@
+﻿namespace NameBook
+{
+    public class NameBook
+    {
+        //Create a class or method that accepts full name, phone number, and email
+        //Maybe an address, social media, and birthday (and notes if we feeling risky)
+        //Create a method or dictionary to add to an empty list or for each individual
+        //Search or print and sort all contacts in the namebook
+        //A way to delete or change a contact
+        //A way to list all the contacts
+        //Make a crazy intellisense like your phone does to automatically know which contacts you possibly refer to
+
+    }
+}
---
+++ ContactBook/ContactBook/NameBookTest/NameBookTest.cs
@@ -0,0 +1,113 @@
+﻿namespace NameBookTest
+{
+    [TestClass]
+    public sealed class NameBookTest
+    {
+        [TestMethod]
+        public void AccessNameBookList()
+        {
+            //Arrange
+            //Hard Coded Values
+            //Act
+            //Using the method to implement test
+            //Assert
+            //Assert true or whatever for actual vs expected
+        }
+        [TestMethod]
+        public void VerifyInitialListIsEmpty()
+        {
+        }
+        [TestMethod]
+        public void AddContactName()
+        {
+        }
+        [TestMethod]
+        public void VerifyCorrectContactNameType()
+        {
+        }
+        [TestMethod]
+        public void AddContactPhoneNumber()
+        {
+        }
+        [TestMethod]
+        public void VerifyCorrectContactPhoneNumberType()
+        {
+        }
+        [TestMethod]
+        public void VerifyCorrectPhoneNumberToName()
+        {
+        }
+        [TestMethod]
+        public void AddContactEmail()
+        {
+        }
+        [TestMethod]
+        public void VerifyCorrectContactEmailType()
+        {
+        }
+        [TestMethod]
+        public void VerifyCorrectEmailToName()
+        {
+        }
+        [TestMethod]
+        public void ChangeContactName()
+        {
+        }
+        [TestMethod]
+        public void ChangeContactPhoneNumber()
+        {
+        }
+        [TestMethod]
+        public void ChangeContactEmail()
+        {
+        }
+        [TestMethod]
+        public void RemoveContactName()
+        {
+        }
+        [TestMethod]
+        public void RemoveContactPhoneNumber()
+        {
+        }
+        [TestMethod]
+        public void RemoveContactEmail()
+        {
+        }
+        [TestMethod]
+        public void SearchByContactName()
+        {
+        }
+        [TestMethod]
+        public void SearchIfContactNameExists()
+        {
+        }
+        [TestMethod]
+        public void SearchByPhoneNumber()
+        {
+        }
+        [TestMethod]
+        public void SearchByEmail()
+        {
+        }
+        [TestMethod]
+        public void SortContactNamesByAlphanumeric()
+        {
+        }
+
+        //copilot extra tests
+        /*
+        VerifyUniqueContactNames
+        VerifyUniquePhoneNumbers
+        VerifyUniqueEmails
+        CheckContactCount
+        VerifyNameUpdateReflectsInAllReferences
+        VerifyPhoneNumberUpdateReflectsInAllReferences
+        VerifyEmailUpdateReflectsInAllReferences
+        VerifyErrorOnAddingInvalidContactData
+        VerifyErrorOnDuplicateContactAddition
+        SearchForNonExistentContact
+        VerifyAlphabeticalSortingCaseInsensitive
+        BackupAndRestoreContacts?
+         */
+    }
+}
